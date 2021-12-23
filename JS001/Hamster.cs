62e25ad81a2aee18b0f10a25704a1781ce2baad4
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2021-12-23 10:34:09 +0100

    AJAX example added

---
+++ JS001/Hamster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JS001
+{
+    public class Hamster
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Farbe { get; set; }
+        public string Geschlecht { get; set; }
+        public int Alter { get; set; }
+    }
+
+    public class HamsterRepository
+    {
+        public static IEnumerable<Hamster> GetHamster()
+        {
+            yield return new Hamster() { Id = 1, Name = "Horst", Alter = 1, Farbe = "Grün", Geschlecht = "m" };
+            yield return new Hamster() { Id = 2, Name = "Winfried", Alter = 16, Farbe = "Gelb", Geschlecht = "m" };
+            yield return new Hamster() { Id = 3, Name = "Hans", Alter = 2, Farbe = "Weiß", Geschlecht = "w" };
+            yield return new Hamster() { Id = 4, Name = "Angela", Alter = 3, Farbe = "Grün", Geschlecht = "w" };
+        }
+    }
+}
---
+++ JS001/MyModule.js
@@ -0,0 +1,14 @@
+﻿
+var MyModule = (function () {
+    function writevar(a) {
+        console.log("a:" + a + ", " + typeof (a))
+    }
+    function _help01() {
+
+    }
+    return {
+        "consolelog": writevar,
+        "winfried": { "Name": "Winfried", "Farbe": "Grün" },
+        "minischderpraesidend": {string : writevar(0)}
+    }
+})()
